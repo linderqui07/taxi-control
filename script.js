80a57ea63bd92828ce2a5253f59d5cd7faf66c27
Linder Quizhpe <114976747+linderqui07@users.noreply.github.com>
2026-04-05 17:55:16 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+input, select {
+    width: 100%;
+    padding: 10px;
+    margin-top: 10px;
+    background: #020617;
+    border: 1px solid #334155;
+    border-radius: 6px;
+    color: white;
+}
+
+button {
+    width: 100%;
+    padding: 12px;
+    margin-top: 10px;
+    background: #2563eb;
+    border: none;
+    border-radius: 6px;
+    color: white;
+}
+
+button:hover {
+    opacity: 0.9;
+}
+
+.pantalla {
+    margin-top: 20px;
+}
+
+.oculto {
+    display: none;
+}
